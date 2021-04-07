12070bad2216cd4f59cff3bc966ebe3f9e4281d4
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-04-07 20:52:02 +0200

    add model and application properties

---
+++ src/main/java/com/example/todoo/model/Priority.java
@@ -0,0 +1,5 @@
+package com.example.todoo.model;
+
+public enum Priority {
+    LOW, MEDIUM, HIGH
+}
---
+++ src/main/java/com/example/todoo/model/Todo.java
@@ -0,0 +1,45 @@
+package com.example.todoo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String description;
+    private Priority priority;
+
+    public Todo(Long id, String description, Priority priority) {
+        this.id = id;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+}
